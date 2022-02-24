0d09c368f55c4dfcac04e5e7ec78ef683d5f63d6
Byron Duarte <byronduarte95@gmail.com>
2022-02-23 21:41:52 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { RideSidebarComponent } from './shared/ride-sidebar/ride-sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    RideSidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/ride-sidebar/ride-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-sidebar',
+  templateUrl: './ride-sidebar.component.html',
+  styleUrls: ['./ride-sidebar.component.css']
+})
+export class RideSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
