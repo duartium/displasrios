9b748ffb75174939f277032d819502f981f6080b
Byron Duarte <byronduarte95@gmail.com>
2022-07-23 12:09:25 -0500

    funcionalidad de despliegue en menu sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -6,7 +6,10 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  @ViewChild("acordion_menu") acordion_menu: ElementRef;
+  @ViewChild("acordion_admin") acordion_admin: ElementRef;
+  @ViewChild("acordion_pedidos") acordion_pedidos: ElementRef;
+  @ViewChild("acordion_reportes") acordion_reportes: ElementRef;
+  
 
   constructor(private renderer2: Renderer2) { }
 
@@ -16,21 +19,24 @@ export class SidebarComponent implements OnInit {
 
   openSubmenu(name: string){
   let element: ElementRef;
-
+  let submenu: HTMLElement;
   switch (name){
-    case 'administrar':
-      element = this.acordion_menu.nativeElement.querySelector('.submenu');
+    case 'administrar': element = this.acordion_admin;
       break;
-    }  
-
-    if((this.acordion_menu.nativeElement as HTMLElement).classList.contains('show')){
-      this.renderer2.setStyle(element, 'display', 'none');
-      this.renderer2.removeClass(this.acordion_menu.nativeElement, 'show');
-      console.log('SI');
+    case 'pedidos': element = this.acordion_pedidos;
+        break;
+    case 'reportes': element = this.acordion_reportes;
+        break;
+        
+  }  
+    
+    submenu = element.nativeElement.querySelector('.submenu');
+    if((element.nativeElement as HTMLElement).classList.contains('show')){
+      this.renderer2.setStyle(submenu, 'display', 'none');
+      this.renderer2.removeClass(element.nativeElement, 'show');
     }else{
-      this.renderer2.setStyle(element, 'display', 'block');
-      this.renderer2.addClass(this.acordion_menu.nativeElement, 'show');
-      console.log('NO');
+      this.renderer2.setStyle(submenu, 'display', 'block');
+      this.renderer2.addClass(element.nativeElement, 'show');
     }
       
   }
