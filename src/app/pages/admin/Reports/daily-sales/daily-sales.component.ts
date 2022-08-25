44635be2d605be6ade88fe74d71ba8c6b865710f
Byron Duarte <byronduarte95@gmail.com>
2022-08-24 23:33:13 -0500

    Por fiiiin se muestra el gráfico para reportes

--- src/app/pages/admin/Reports/daily-sales/daily-sales.component.ts
+++ src/app/pages/admin/Reports/daily-sales/daily-sales.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-daily-sales',
   templateUrl: './daily-sales.component.html',
   styleUrls: ['./daily-sales.component.css']
 })
-export class DailySalesComponent implements OnInit {
+export class DailySalesComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  name = 'Angular   6';
+  canvas: any;
+  ctx: any;
+  myChart: Chart;
+  @ViewChild('mychart') mychart;
+  data: any = [];
+  constructor(){}
 
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
+
+  ngAfterViewInit() {
+    this.canvas = this.mychart.nativeElement; 
+    this.ctx = this.canvas.getContext('2d');
+
+    this.data = {
+      labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+      ],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    };
+
+    this.myChart = new Chart(this.ctx, {
+      type: 'pie',
+      data: this.data,
+     
+    });
+  }
+
+  refresh(){
+    //chart.data.labels.push(label);
+    this.myChart.data.datasets[0].data = [];
+    this.myChart.data.datasets[0].data = [100, 400, 100];
+    console.log(this.myChart.data.datasets);
+    this.myChart.update();
+  }
+
+
 
 }
