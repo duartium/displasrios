ca64f47315e2d4561947bf219f340ab4866a50fc
Byron Duarte <byronduarte95@gmail.com>
2022-10-01 15:50:01 -0500

    Se corrige la agregación del nuevo item en tiempo real

--- src/app/pages/admin/orders-of-day/orders-of-day.component.ts
+++ src/app/pages/admin/orders-of-day/orders-of-day.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SummaryOrderOfDay } from 'src/app/Dtos/OrderReceivableDto.model';
 import { OrderService } from 'src/app/services/order.service';
@@ -14,17 +14,22 @@ export class OrdersOfDayComponent implements OnInit {
   summaryOrders: SummaryOrderOfDay[];
   constructor(private orderService: OrderService,
     private router: Router,
-    private signalrService: SignalrService) { }
+    private signalrService: SignalrService,
+    private cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    this.getSummaryOrdersOfDay();
     this.signalrService.eventNotifica.subscribe((newOrder: SummaryOrderOfDay) => {
       console.log('newOrder', newOrder);
-      let newSummaryOrders = [...this.summaryOrders];
-      newSummaryOrders.unshift(newOrder);
-      this.summaryOrders = newSummaryOrders;
+
+      this.summaryOrders.unshift(newOrder);
       console.log('summaryOrders', this.summaryOrders);
+      this.cdRef.detectChanges();
     });
+    this.getSummaryOrdersOfDay();
+  }
+
+  identify(index, item) {
+    return index;
   }
 
   getSummaryOrdersOfDay(){
