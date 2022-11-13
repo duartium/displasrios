120dc0a48a462f42f8397cd5a1cb41f8cbe59a2d
Byron Duarte <byronduarte95@gmail.com>
2022-11-12 22:18:58 -0500

    Se corrige goToOrder que renderizaba en la misma pagina de pedidos

--- src/app/pages/admin/orders-of-day/orders-of-day.component.ts
+++ src/app/pages/admin/orders-of-day/orders-of-day.component.ts
@@ -24,6 +24,7 @@ export class OrdersOfDayComponent implements OnInit {
       this.summaryOrders.unshift(newOrder);
       console.log('summaryOrders', this.summaryOrders);
       this.cdRef.detectChanges();
+      this.getSummaryOrdersOfDay();
     });
     this.getSummaryOrdersOfDay();
   }
